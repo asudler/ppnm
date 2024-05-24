f190192f60686d122a21813cae8381100b437b5a
ajs <aidanjsudler@gmail.com>
2024-05-24 13:13:30 +0000

    finished monte carlo

--- homework/monte-carlo/main.cs
+++ homework/monte-carlo/main.cs
@@ -46,20 +46,30 @@ public static class main {
         Error.Write($"sol = {sol}, err = {err} ");
         Error.Write($"(real err = {Math.Abs(sol - 4)})\n");
 
-        // now we will see if the error scales with the
-        // square root of the number of points by
         // calculating some wacky integral
         Func<double[], double> gamma = x => {
            return 1/(1 - Math.Cos(x[0])*Math.Cos(x[1])
                *Math.Cos(x[2]))/Math.Pow(Math.PI, 3); 
         }; // gamma-ish function
 
-        double realans = 1.393203929685676;
         double[] zero = {0,0,0}, 
             pis = {Math.PI, Math.PI, Math.PI};
+        Error.Write($"some wacky gamma-ish function: ");
+        (sol, err) = plainmc(gamma, zero, pis, n);
+        Error.Write($"sol = {sol}, err = {err} ");
+        double realans = 1.393203929685676;
+        Error.Write($"(real err = {Math.Abs(sol - realans)})\n");
+
+        // we should see how error scales with n
+        // perhaps use the 2D Gaussian function...?
+        Console.Write("columns are: ");
+        Console.Write("(1) n, ");
+        Console.Write("(2) plainmc sol, ");
+        Console.Write("(3) plainmc err, ");
+        Console.Write("(4) real err\n");
         for(int i = nmin; i <= nmax; i += skip) {
-            (sol, err) = plainmc(gamma, zero, pis, i);
-            Write($"{i}\t{sol}\t{err}\t{Math.Abs(sol - realans)}\n");
+            (sol, err) = plainmc(f2, ll, ur, i);
+            Console.Write($"{i}\t{sol}\t{err}\t{Math.Abs(sol - help)})\n");
         }
     } // taska
 
@@ -109,33 +119,51 @@ public static class main {
         Error.Write($"(real errh = {Math.Abs(solh - 4)}, ");
         Error.Write($"real errl = {Math.Abs(soll - 4)})\n");
 
-        // calculating some wacky integral
-        Func<double[], double> gamma = x => {
-           return 1/(1 - Math.Cos(x[0])*Math.Cos(x[1])
-               *Math.Cos(x[2]))/Math.Pow(Math.PI, 3); 
-        }; // gamma-ish function
-
-        double[] corner1 = {1,1,1}, corner2 = {3, 3, 3};
-        Write("columns are: ");
-        Write("(1) n, ");
-        Write("(2) plain mc sol, ");
-        Write("(3) halton mc sol, ");
-        Write("(4) lattice mc sol, ");
-        Write("(5) plain mc err, ");
-        Write("(6) quasi mc method difference (err)\n");
+        // we should see how error scales with n
+        // perhaps use the 2D Gaussian function...?
+        Console.Write("columns are: ");
+        Console.Write("(1) n, ");
+        Console.Write("(2) haltonmc sol, ");
+        Console.Write("(3) latticemc sol, ");
+        Console.Write("(4) quasimc method difference (err), ");
+        Console.Write("(5) real haltonmc err, ");
+        Console.Write("(5) real latticemc err\n");
         for(int i = nmin; i <= nmax; i += skip) {
-            (double sol, double perr) = plainmc(gamma, corner1, corner2, i);
-            (solh, soll, err) = quasimc(gamma, corner1, corner2, i);
-            Write($"{i}\t{sol}\t{solh}\t{soll}\t");
-            Write($"{err}\t{Math.Abs(solh - soll)}\n");
+            (solh, soll, err) = quasimc(f2, ll, ur, i);
+            Console.Write($"{i}\t{solh}\t{soll}\t");
+            Console.Write($"{Math.Abs(solh - soll)}\t");
+            Console.Write($"{Math.Abs(solh - help)}\t");
+            Console.Write($"{Math.Abs(soll - help)}\n");
         }
     } // taskb
-    
+
+    public static void taskc(string[] args) {
+        int n = 0;
+        foreach(var arg in args) {
+            var words = arg.Split(":");
+            if(words[0] == "-n") n = int.Parse(words[1]);
+        }
+        if(n == 0) throw new Exception("bad input args");
+        
+        Func<double[], double> f1 = x => {
+            if(Math.Sqrt(x[0]*x[0] + x[1]*x[1]) <= 1) return 1;
+            else return 0;
+        }; // unit circle function
+
+        // testing our stratified monte carlo integration routine
+        Error.Write($"montecarlo: stratmc tests (n = {n})\n");
+        Error.Write($"unit circle area: ");
+        double[] ll = {-1.2,-1.2}, ur = {1.2,1.2};
+        (double sol, double err) = stratmc(f1, ll, ur, n, true);
+        Error.Write($"sol = {sol}, err = {err} ");
+        Error.Write($"(real err = {Math.Abs(Math.PI - sol)})\n");
+    } // taskc
+
     public static int Main(string[] args) {
         foreach(string arg in args) {
             if(arg == "-taska") taska(args);
             if(arg == "-taskb") taskb(args);
-//            if(arg == "-taskc") taskc(args);
+            if(arg == "-taskc") taskc(args);
         }
         return 0;
     } // Main
--- homework/monte-carlo/montecarlo.cs
+++ homework/monte-carlo/montecarlo.cs
@@ -5,7 +5,8 @@ public static class montecarlo {
      * plain Monte Carlo integration algorithm
      * see Fedorov p. 75 */
     public static (double, double) plainmc
-    (Func<double[],double> f, double[] a, double[] b, int N) {
+    (Func<double[],double> f, double[] a, double[] b, int N,
+     bool write=false) {
         int dim = a.Length; double V = 1;
         for(int i = 0; i < dim; i++) V *= b[i] - a[i];
         double sum1 = 0, sum2 = 0;
@@ -14,10 +15,11 @@ public static class montecarlo {
         for(int i = 0; i < N; i++) {
             for(int k = 0; k < dim; k++) {
                 x[k] = a[k] + rnd.NextDouble()*(b[k] - a[k]);
+                if(write) Console.Write($"{x[k]}\t");
             }
+            if(write) Console.Write($"\n");
             double fx = f(x);
-            sum1 += fx;
-            sum2 += fx*fx;
+            sum1 += fx; sum2 += fx*fx;
         }
         double mean = sum1/N, sigma = Math.Sqrt(sum2/N - mean*mean);
         return (mean*V, sigma*V/Math.Sqrt(N));
@@ -45,6 +47,107 @@ public static class montecarlo {
         double meanh = sumh/N, meanl = suml/N, sigma = Math.Abs(meanh - meanl);
         return (meanh*V, meanl*V, sigma*V);
     } // quasimc
+
+    /* stratmc:
+     * recursive stratified monte carlo sampling algorithm, 
+     * it probably could be done more eloquently... */
+    public static (double, double) stratmc
+    (Func<double[],double> f, double[] a, double[] b, int N,
+     bool write=false, int nmin=100) {
+        if(a.Length != b.Length) throw new Exception("bad limit dimensions");
+        if(N < nmin) return plainmc(f, a, b, N, write);
+        
+        // (1) sample nmin points and estimate the integral and variance
+        // (2) figure out which dimension has largest variance
+        int dim = a.Length; double V = 1;
+        for(int i = 0; i < dim; i++) V *= b[i] - a[i];
+        double[] sum1 = new double[dim], sum1v = new double[dim],
+            sum2 = new double[dim], sum2v = new double[dim];
+        int[] count1 = new int[dim], count2 = new int[dim];
+        var rnd = new Random();
+        for(int i = 0; i < nmin; i++) {
+            double[] x = new double[dim];
+            for(int j = 0; j < dim; j++) {
+                x[j] = a[j] + rnd.NextDouble()*(b[j] - a[j]);
+                if(write) Console.Write($"{x[j]}\t");
+            }
+            if(write) Console.Write("\n");
+            double fx = f(x);
+            for(int j = 0; j < dim; j++) {
+                if(x[j] < a[j]/2 + b[j]/2) { 
+                    sum1[j] += fx; 
+                    sum1v[j] += fx*fx;
+                    count1[j] += 1;
+                }
+                else { sum2[j] += fx; sum2v[j] += fx*fx; count2[j] += 1; }
+            }
+        }
+        double help1 = 0, help2 = 0; int vdim = 0, side = 0;
+        for(int i = 0; i < dim; i++) {
+            double[] sigma = new double[dim*2];
+            sigma[2*i] = Math.Sqrt(sum1v[i]/count1[i] 
+                    - sum1[i]*sum1[i]/count1[i]/count1[i]);
+            sigma[2*i + 1] = Math.Sqrt(sum2v[i]/count2[i]  
+                - sum2[i]*sum2[i]/count2[i]/count2[i]);
+            if(count1[i] == 0) sigma[2*i] = 0;  
+            if(count2[i] == 0) sigma[2*i + 1] = 0;
+            if(sigma[2*i] > help1) {
+                help1 = sigma[2*i];
+                help2 = sigma[2*i + 1];
+                vdim = i;
+                side = 0;
+            }
+            if(sigma[2*i + 1] > help1) {
+                help1 = sigma[2*i + 1];
+                help2 = sigma[2*i];
+                vdim = i;
+                side = 1;
+            }
+        }
+        
+        // (3) subdivide the volume along this dimension (vdim)
+        double[] aa = new double[dim], bb = new double[dim];
+        Array.Copy(a, aa, dim); Array.Copy(b, bb, dim);
+        aa[vdim] = a[vdim]/2 + b[vdim]/2;
+        bb[vdim] = a[vdim]/2 + b[vdim]/2;
+
+        // (4) divide the remaining points between the two volumes
+        // proportional to the sub-variances. if the subvariances
+        // are equally 0, then the points should be divided evenly
+        double factor;
+        if(help1 == 0 && help2 == 0) factor = 0.5;
+        else factor = help1/(help1 + help2);
+        int n1 = (int)(factor*(N - nmin)), n2 = N - nmin - n1;
+
+        // (5) dispatch recursive calls to the subvolumes
+        // and get that result babeyyyyy
+        double sol1 = 0, sol2 = 0, err1 = 0, err2 = 0;
+        if(side == 0) {
+            if(n1 != 0 && n2 != 0) {
+                (sol1, err1) = stratmc(f, a, bb, n1, write);
+                (sol2, err2) = stratmc(f, aa, b, n2, write);
+            }
+            if(n1 != 0 && n2 == 0) {
+                (sol1, err1) = stratmc(f, a, bb, n1, write);
+                (sol2, err2) = (V/2*sum2[vdim]/count2[vdim], 
+                    V/2*Math.Sqrt(sum2v[vdim]/count2[vdim]
+                    - sum2[vdim]*sum2[vdim]/count2[vdim]/count2[vdim]));
+            }
+        }
+        else {
+            if(n1 != 0 && n2 != 0) {
+                (sol1, err1) = stratmc(f, aa, b, n1, write);
+                (sol2, err2) = stratmc(f, a, bb, n2, write);
+            }
+            if(n1 != 0 && n2 == 0) {
+                (sol1, err1) = stratmc(f, aa, b, n1, write);
+                (sol2, err2) = (V/2*sum1[vdim]/count1[vdim],
+                    V/2*Math.Sqrt(sum1v[vdim]/count1[vdim]
+                    - sum1[vdim]*sum1[vdim]/count1[vdim]/count1[vdim]));
+            }
+        }
+        return (sol1 + sol2, Math.Sqrt(err1*err1 + err2*err2));
+    } // stratmc
 
     /* corput:
      * van der Corput sequence over the unit interval 
